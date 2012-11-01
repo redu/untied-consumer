dc3cb1b4e3df57290b62c4722e5e01181392446a
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-11-01 10:27:46 -0300

    Refactoring Consumer::Worker; Adding daemonize method to run the worker in background

--- lib/untied-consumer/worker.rb
+++ lib/untied-consumer/worker.rb
@@ -2,19 +2,58 @@
 module Untied
   module Consumer
     class Worker
-      def initialize(opts)
-        @channel = opts[:channel]
+      def initialize(opts={})
         @queue_name = opts[:queue_name] || ""
-        @consumer = opts[:consumer] || Processor.new
-        @exchange = opts[:exchange]
-
-        Consumer.config.logger.info "Worker initialized and listening"
+        @consumer = opts[:processor] || Processor.new
       end
 
+      # Initializes the worker and calls the start method
+      def self.start(opts={})
+        worker = new(opts)
+        worker.start
+        worker
+      end
+
+      # Daemonizes the current worker. Remember you'll need the daemons Gem
+      # in order to this method work correctly.
+      #
+      # Options:
+      #   :pids_dir => '/some/dir' Absolute path to the dir where pid files will live
+      #   :log_dir => '/some/dir' Absolute path to the dir where log files will live
+      def daemonize(opts={})
+        require 'daemons' # just in case
+
+        config = {
+          :backtrace  => true,
+          :log_output => true,
+          :dir_mode   => :normal,
+          :dir        => opts[:pids_dir],
+          :log_dir    => nil,
+        }.merge(opts)
+
+        if !(config[:dir] && config[:log_dir])
+          raise ArgumentError "You need to provide pids_dir and log_dir"
+        end
+
+        FileUtils.mkdir_p(config[:dir])
+        FileUtils.mkdir_p(config[:log_dir])
+
+        @worker = self
+        Daemons.run_proc('untiedc', config) { @worker.start }
+      end
+
+      # Listens to the mssage bus for relevant events. This method blocks the
+      # current thread.
       def start
-        @channel.queue(@queue_name, :exclusive => true) do |queue|
-          queue.bind(@exchange, :routing_key => "untied.#").subscribe do |h,p|
-            safe_process { @consumer.process(h,p) }
+        AMQP.start do |connection|
+          channel  = AMQP::Channel.new(connection)
+          exchange = channel.topic("untied", :auto_delete => true)
+
+          channel.queue(@queue_name, :exclusive => true) do |queue|
+            queue.bind(exchange, :routing_key => "untied.#").subscribe do |h,p|
+              Consumer.config.logger.info "Worker initialized and listening"
+              safe_process { @consumer.process(h,p) }
+            end
           end
         end
       end
